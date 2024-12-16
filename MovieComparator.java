d3d8cf5b20332e445a0e7b6a4020f23427251eca
Bcsay04 <bakaryceesay183@gmail.com>
2024-12-15 20:06:32 -0600

    Update MovieComparator.java

--- MovieComparator.java
+++ MovieComparator.java
@@ -1,12 +1,10 @@
-import java.util.Comparator;
+mport java.util.Comparator;
 
 public class MovieComparator implements Comparator<Movie> {
-
-    public int compare(Movie r1, Movie r2) {
-        
-
-        
-        return 1;
+    @Override
+    public int compare(Movie m1, Movie m2) {
+        double wAvg1 = MovieReccom.computeWeightedAverage(m1);
+        double wAvg2 = MovieReccom.computeWeightedAverage(m2);
+        return Double.compare(wAvg2, wAvg1);  // Sort in descending order by weighted average
     }
-    
 }
